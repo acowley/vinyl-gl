59b4dafe3ff9a95117d61103c66e83807f082edc
Anthony Cowley <acowley@gmail.com>
2013-06-10 21:52:15 -0400

    Don't export Viewport due to questionable taste.

--- src/Graphics/VinylGL.hs
+++ src/Graphics/VinylGL.hs
@@ -1,7 +1,5 @@
 module Graphics.VinylGL (module Graphics.VinylGL.Uniforms,
-                         module Graphics.VinylGL.Vertex,
-                         module Graphics.VinylGL.Viewport) 
+                         module Graphics.VinylGL.Vertex) 
   where
 import Graphics.VinylGL.Uniforms
 import Graphics.VinylGL.Vertex
-import Graphics.VinylGL.Viewport
