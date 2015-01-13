71c62a04579a1804ddf67d5c1f18285ce79c3598
Anthony Cowley <acowley@gmail.com>
2015-01-12 19:04:39 -0500

    Better factored haddock upload script.

--- hackagedocs.sh
+++ hackagedocs.sh
@@ -1,18 +1,19 @@
 #!/bin/sh
 # Based on https://gist.github.com/Fuuzetsu/8276421
 # Usage: sh hackagedocs.sh 0.7.1 UserName Password
+PKG_NAME=vinyl-gl
 
 cabal configure && cabal build && cabal haddock --hyperlink-source --html-location='http://hackage.haskell.org/package/$pkg/docs' \
                                     --contents-location='http://hackage.haskell.org/package/$pkg'
 S=$?
 if [ "${S}" -eq "0" ]; then
     cd "dist/doc/html"
-    DDIR="vinyl-gl-${1}-docs"
-    cp -r "vinyl-gl" "${DDIR}" && tar -c -v -z --format ustar -f "${DDIR}.tar.gz" "${DDIR}"
+    DDIR="${PKG_NAME}-${1}-docs"
+    cp -r "${PKG_NAME}" "${DDIR}" && tar -c -v -z --format ustar -f "${DDIR}.tar.gz" "${DDIR}"
     CS=$?
     if [ "${CS}" -eq "0" ]; then
         echo "Uploading to Hackage…"
-        curl -X PUT -H 'Content-Type: application/x-tar' -H 'Content-Encoding: gzip' --data-binary "@${DDIR}.tar.gz" "http://${2}:${3}@hackage.haskell.org/package/vinyl-gl-${1}/docs"
+        curl -X PUT -H 'Content-Type: application/x-tar' -H 'Content-Encoding: gzip' --data-binary "@${DDIR}.tar.gz" "http://${2}:${3}@hackage.haskell.org/package/${PKG_NAME}-${1}/docs"
         exit $?
     else
         echo "Error when packaging the documentation"
